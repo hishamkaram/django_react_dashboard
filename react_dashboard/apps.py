2cc9e735c3f89e3e406cf3423a385536b2dd5b13
Unknown <ibrahim.a.hamid@gmail.com>
2019-01-01 12:36:36 +0200

    Initial Setup of Django and React

---
+++ react_dashboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReactDashboardConfig(AppConfig):
+    name = 'react_dashboard'
---
+++ react_dashboard/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from .views import HomePage
+
+urlpatterns = [
+    url(r'^', HomePage.as_view()),
+]
